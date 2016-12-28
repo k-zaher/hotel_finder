f4bc81e76daec1a3d8bee70f7a788a6e6adfc7ef
Kareem Diaa <kareem.diaa@gmail.com>
2016-12-28 17:01:24 +0200

    Fixing Code Style

--- mw/spec/controllers/api/v1/sessions_controller_spec.rb
+++ mw/spec/controllers/api/v1/sessions_controller_spec.rb
@@ -2,36 +2,36 @@ require 'rails_helper'
 
 RSpec.describe Api::V1::SessionsController, type: :controller do
   describe 'POST #Create' do
-    let(:user) {User.first}
+    let(:user) { User.first }
     before :each do
       request.env['devise.mapping'] = Devise.mappings[:user]
     end
 
     context 'Email or Password is not provided' do
       it 'returns 401' do
-        post :create, params: {user: {email: "kareem@test.com"}}
+        post :create, params: { user: { email: 'kareem@test.com' } }
         expect(response).to have_http_status(401)
       end
     end
 
     context 'Email or Password is not valid' do
       it 'returns 401' do
-        post :create, params: { user: {email: 'test@test.com', password: 'test'} }
+        post :create, params: { user: { email: 'test@test.com', password: 'test' } }
         expect(response).to have_http_status(401)
       end
     end
 
     context 'Email and Password are valid' do
       it 'returns 200 ok' do
-        post :create, params: { user: {email: user.email, password: 'test@1234'} }
+        post :create, params: { user: { email: user.email, password: 'test@1234' } }
         expect(response).to have_http_status(200)
-        expect(JSON.parse(response.body).keys).to eq(["authentication_token"])
+        expect(JSON.parse(response.body).keys).to eq(['authentication_token'])
       end
     end
   end
 
   describe 'DELTE #Destroy' do
-    let(:user) {User.first}
+    let(:user) { User.first }
     before :each do
       request.env['devise.mapping'] = Devise.mappings[:user]
       sign_in user
